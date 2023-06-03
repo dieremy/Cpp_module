0b8308031e01ef749196399db7f1f85832f2e3c4
Roberto Remy Garcia <robegarc@e3r6p2.42roma.it>
2023-06-03 13:58:48 +0200

    done ex01

---
+++ cpp_module_02/ex01/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: robegarc <robegarc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/03 12:29:14 by robegarc          #+#    #+#             */
+/*   Updated: 2023/06/03 12:30:01 by robegarc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "include/Fixed.hpp"
+
+int main( void )
+{
+	Fixed a;
+	Fixed const b( 10 );
+	Fixed const c( 42.42f );
+	Fixed const d( b );
+	a = Fixed( 1234.4321f );
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	
+	return (0);
+}
