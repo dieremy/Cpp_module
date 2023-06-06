04287a68e051e60ca1316e4af9e44b2c854884cd
Roberto Remy Garcia <robegarc@e4r6p2.42roma.it>
2023-06-06 16:55:42 +0200

    orderingfile

--- cpp_module_01/ex03/srcs/HumanA.cpp
+++ cpp_module_01/ex03/srcs/HumanA.cpp
@@ -6,16 +6,14 @@
 /*   By: robegarc <robegarc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 17:04:17 by robegarc          #+#    #+#             */
-/*   Updated: 2023/05/28 15:13:30 by robegarc         ###   ########.fr       */
+/*   Updated: 2023/06/06 16:52:40 by robegarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/HumanA.hpp"
 
 HumanA::HumanA( std::string name, Weapon &Weapon )
-{
-    // humana bob needs name and kind of weapon
-	
+{	
     this->_name = name;
     this->Weap = &Weapon;
 	this->Weap->getType();
@@ -23,12 +21,11 @@ HumanA::HumanA( std::string name, Weapon &Weapon )
 
 HumanA::~HumanA()
 {
+	;
 }
 
 int	HumanA::attack( void )
 {
-    //    <name> attacks with their <weapon type>
-	
 	std::cout << CYAN << this->_name;
 	std::cout << " attacks with their ";
 	std::cout << this->Weap->getType() << NONE << std::endl;
