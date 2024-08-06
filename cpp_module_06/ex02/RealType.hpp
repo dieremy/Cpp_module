956495ee8cf1c2bdea16017d6925201d1d5ab480
rgarcia <robertoremyg@gmail.com>
2024-08-06 12:27:56 -0400

    Done cpp_module06

---
+++ cpp_module_06/ex02/RealType.hpp
@@ -0,0 +1,21 @@
+//
+// Created by test on 8/6/24.
+//
+
+#ifndef CPP_MODULE_REALTYPE_HPP
+#define CPP_MODULE_REALTYPE_HPP
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+class Base {
+public:
+	virtual ~Base() {}
+};
+
+class A : public Base {};
+class B : public Base {};
+class C : public Base {};
+
+#endif //CPP_MODULE_REALTYPE_HPP
---
+++ cpp_module_06/ex02/main.cpp
@@ -0,0 +1,65 @@
+//
+// Created by test on 8/6/24.
+//
+
+#include "RealType.hpp"
+
+Base* generate() {
+	std::cout << "Generated class: ";
+
+	int r = rand() % 3;
+	if (r == 0) {
+		std::cout << "A";
+		return new A();
+	} else if (r == 1) {
+		std::cout << "B";
+		return new B();
+	} else {
+		std::cout << "C";
+		return new C();
+	}
+}
+
+void identify(Base* x) {
+	std::cout << "Identified by pointer: ";
+
+	if (dynamic_cast<A*>(x))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(x))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(x))
+		std::cout << "C" << std::endl;
+	else
+		std::cerr << "bad cast" << std::endl;
+}
+
+void identify(Base& x) {
+	std::cout << "Identified by reference: ";
+
+	if (&x == dynamic_cast<A*>(&x)) {
+		std::cout << "A" << std::endl;
+	} else if (&x == dynamic_cast<B*>(&x)) {
+		std::cout << "B" << std::endl;
+	} else if (&x == dynamic_cast<C*>(&x)) {
+		std::cout << "C" << std::endl;
+	} else {
+		std::cerr << "bad cast" << std::endl;
+	}
+}
+
+int main() {
+	srand(time(NULL));
+
+	Base* x = generate();
+	std::cout << " -> pointer" << std::endl;
+
+	Base* y = generate();
+	Base& z = *y;
+	std::cout << " -> reference" << std::endl;
+
+	identify(x);
+	identify(z);
+
+	delete x;
+	delete y;
+}
